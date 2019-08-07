26fc78bf10d16881ce08b99da6d136d78a639152
c0ld574rf15h <c0ld574rf15h@gmail.com>
2019-08-07 20:21:23 +0900

    2606

---
+++ 2606.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int virus = 0;
+bool visited[101];
+vector<vector<int> > v(101);
+
+void dfs(int s) {
+    visited[s] = true;
+    virus += 1;
+    for(int i=0;i<v[s].size();i++)
+        if(!visited[v[s][i]])
+            dfs(v[s][i]);
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    memset(visited, false, sizeof(visited));
+    int n, m; cin >> n >> m;
+    for(int i=0;i<m;i++) {
+        int x, y; cin >> x >> y;
+        v[x].push_back(y);
+        v[y].push_back(x);
+    }
+    dfs(1);
+    cout << virus-1 << '\n';
+    return 0;
+}
