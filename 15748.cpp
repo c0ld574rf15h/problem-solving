64a6c6cd6d207bb3e36f1a0e139f6b141cdee0da
c0ld574rf15h <c0ld574rf15h@gmail.com>
2019-08-20 14:00:56 +0900

    15748

--- 15748.cpp
+++ 15748.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
-#include <string>
+#include <functional>
+#include <algorithm>
+#include <vector>
 using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+ll L, N, f, b;
+vector<pll> stop;
 
 int main(void) {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int n; cin >> n;
-  int cur = 666, cnt = 1;
-  while(true) {
-    if(cur.to_string().find("666") != string::npos) {
-      if(cnt==n) {
-        cout << cur << '\n';
-        break;
-      } cnt++;
+  cin >> L >> N >> f >> b;
+  for(int i=0;i<N;++i) {
+    ll a, b; cin >> a >> b;
+    stop.push_back(make_pair(b, -a));
   }
+  sort(stop.begin(), stop.end(), greater<pll>());
+  ll cur = 0, ans = 0;
+  for(int i=0;i<N;++i) {
+    ll next = -stop[i].second;
+    if(cur > next) continue;
+    ans += (f-b)*(next-cur)*stop[i].first;
+    cur = next;
+  }
+  cout << ans << '\n';
   return 0;
 }
